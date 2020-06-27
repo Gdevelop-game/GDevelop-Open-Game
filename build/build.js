bf52714a4a87e766d7cf8268f667df9c8a025d5a
arthuro555 <arthur.pacaud@hotmail.fr>
2020-06-27 13:38:39 +0200

    Make game a PWA

--- build/build.js
+++ build/build.js
@@ -1,37 +1,44 @@
 // Export the GDevelop game to JS
 
-const fs = require("fs");
+const fs = require("fs-extra");
 
 const buildType = process.argv.slice(2)[0];
 const electron = buildType === "electron";
 let exportPath = "./gdExport";
 if(electron) exportPath += "/app";
 
-const copy = (origin, out) => {
-  const file = fs.readFileSync(origin);
-  fs.writeFileSync(out, file);
-}
 
 require("gdexporter").exporter("../src/game.json", "gdExport", buildType)
 .then(() => {
-  console.log("Use custom index.html");
-  copy(
+  console.log("Add custom index.html");
+  fs.copySync(
     "./customHtml/index.html", 
     exportPath + "/index.html"
   );
 })
 .then(() => {
   console.log("Add custom loading screen");
-  copy(
+  fs.copySync(
     "./customLoadingScreen/loadingscreen-pixi-renderer.js", 
     exportPath + "/pixi-renderers/loadingscreen-pixi-renderer.js"
   );
 })
 .then(() => {
-  console.log("Add Cache Service Worker");
-  copy(
-    "./workboxCache/sw.js", 
+  console.log("Make game PWA Ready");
+  fs.copySync(
+    "./pwa/sw.js", 
     exportPath + "/sw.js"
+  );
+  fs.copySync(
+    "./pwa/manifest.webmanifest", 
+    exportPath + "/manifest.webmanifest"
+  );
+})
+.then(()=> {
+  console.log("Copy Icons");
+  fs.copySync(
+    "./icons", 
+    exportPath + "/icons"
   );
 })
 .then(() => {
