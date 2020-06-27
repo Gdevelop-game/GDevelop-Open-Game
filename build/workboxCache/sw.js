c7b0b332e7737cea112fae2879598fb1f1e95a13
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2020-06-27 11:33:16 +0200

    Add caching (#49)
    To let the game be played offline in bowser

---
+++ build/workboxCache/sw.js
@@ -0,0 +1,2 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+workbox.routing.registerRoute(new RegExp("[\s\S]*"), new workbox.strategies.NetworkFirst());
