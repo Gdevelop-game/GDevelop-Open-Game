a49aa2d1b09a7c287bc33cbe3b0052bdcefe7ac9
The Gem Dev <53819287+the-gem-dev@users.noreply.github.com>
2021-02-17 21:06:03 +0100

    removed build dir

--- build/pwa/sw-local.js
+++
@@ -1,3 +0,0 @@
-// Local (PWA) Worker
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-workbox.routing.registerRoute(new RegExp("[\s\S]*"), new workbox.strategies.StaleWhileRevalidate());
