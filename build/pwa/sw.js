046ae960b580fc6afd65b4330f5b02563593795e
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2021-02-17 22:50:42 +0100

    Move to GDexport plugins (#191)
    * Use gdexport plugins
    
    * Remove yarn lockfile
    
    * Specify node version

--- build/pwa/sw.js
+++
@@ -1,2 +0,0 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-workbox.routing.registerRoute(new RegExp("[\s\S]*"), new workbox.strategies.StaleWhileRevalidate());
